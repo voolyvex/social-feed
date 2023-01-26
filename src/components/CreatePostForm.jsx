4c8357cc11ff68155bc2a85f124fd8607363ae88
voolyvex <1joeknapp@gmail.com>
2023-01-26 11:03:54 -0600

    Add comments for myself in component files

--- src/components/CreatePostForm.jsx
+++ src/components/CreatePostForm.jsx
@@ -1,1 +1,2 @@
 /* In the CreatePostForm component create a form with 2 input fields. There should be an input field for the user’s name, and one for the body of their post. Each input field should be bound to its own state variable.*/
+/* Implement a handleSubmit method for your form, which should pass the entered values to a function on the App.jsx component. That function should create a new post object, add it to the existing array of posts, and update the App.js state variable with the new array. */
